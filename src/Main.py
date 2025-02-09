b6f16fc270db84bb511b243ddc6ec5a4f14b2e99
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-02-09 10:48:28 +0530

    Formatter added

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,11 @@
 from Views.StartView import StartView
 from src.Models.SQLInteraction import SQLInteraction
 
+
 class Program:
     def __init__(self):
         self.App = StartView()
+
 
 # Example usage
 if __name__ == "__main__":
